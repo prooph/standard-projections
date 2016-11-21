d7798dfdc30bf44e400905734bab9ea508a68266
prolic <saschaprolic@googlemail.com>
2016-11-21 17:40:55 +0800

    add all stream projection

---
+++ src/Container/AllStreamProjectionFactory.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * This file is part of the prooph/standard-projections.
+ * (c) 2016-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Prooph\StandardProjections\Container;
+
+use Interop\Config\ConfigurationTrait;
+use Interop\Config\ProvidesDefaultOptions;
+use Interop\Config\RequiresConfig;
+use Interop\Config\RequiresMandatoryOptions;
+use Interop\Container\ContainerInterface;
+use PDO;
+use Prooph\EventStore\Projection\Projection;
+use Prooph\StandardProjections\AllStreamProjection;
+
+class AllStreamProjectionFactory implements ProvidesDefaultOptions, RequiresConfig, RequiresMandatoryOptions
+{
+    use ConfigurationTrait;
+
+    /**
+     * @var array
+     */
+    private $pdoDriverSchemeAliases = [
+        'pdo_mysql'  => 'mysql',
+        'pdo_pgsql'  => 'pgsql',
+    ];
+
+    private $pdoDriverSchemeSeparators = [
+        'pdo_mysql'  => ';',
+        'pdo_pgsql'  => ' ',
+    ];
+
+    public function __invoke(ContainerInterface $container): AllStreamProjection
+    {
+        $config = $container->get('config');
+        $config = $this->options($config);
+
+        $eventStore = $container->get($config['event_store']);
+
+        switch (strtolower($config['type'])) {
+            case 'mysql':
+                $projection = new \Prooph\EventStore\PDO\Projection\MySQLEventStoreProjection(
+                    $eventStore,
+                    $this->getPDOConnection($container, $config),
+                    '$all',
+                    $config['event_streams_table'],
+                    $config['projections_table'],
+                    $config['lock_timeout_ms'],
+                    true
+                );
+                break;
+            case 'postgres':
+                $projection = new \Prooph\EventStore\PDO\Projection\PostgresEventStoreProjection(
+                    $eventStore,
+                    $this->getPDOConnection($container, $config),
+                    '$all',
+                    $config['event_streams_table'],
+                    $config['projections_table'],
+                    $config['lock_timeout_ms'],
+                    true
+                );
+                break;
+            case 'inmemory':
+                $projection = new \Prooph\EventStore\Projection\InMemoryEventStoreProjection(
+                    $eventStore,
+                    '$all',
+                    true
+                );
+                break;
+            default:
+                throw new \InvalidArgumentException('Unknown projection type given');
+        }
+
+        return new AllStreamProjection($projection);
+    }
+
+    public function dimensions(): array
+    {
+        return ['prooph', 'standard_projections'];
+    }
+
+    public function mandatoryOptions(): array
+    {
+        return [
+            'event_store',
+            'type',
+        ];
+    }
+
+    public function defaultOptions(): array
+    {
+        return [
+            'connection_options' => [
+                'driver' => 'pdo_mysql',
+                'user' => 'root',
+                'password' => '',
+                'host' => '127.0.0.1',
+                'dbname' => 'event_store',
+                'port' => 3306,
+            ],
+            'event_streams_table' => 'event_streams',
+            'projections_table' => 'projection',
+            'lock_timeout_ms' => 1000,
+        ];
+    }
+
+    protected function getPDOConnection(ContainerInterface $container, $config): PDO
+    {
+        if (isset($config['connection_service'])) {
+            $connection = $container->get($config['connection_service']);
+        } else {
+            $separator = $this->pdoDriverSchemeSeparators[$config['connection_options']['driver']];
+            $dsn = $this->pdoDriverSchemeAliases[$config['connection_options']['driver']] . ':';
+            $dsn .= 'host=' . $config['connection_options']['host'] . $separator;
+            $dsn .= 'port=' . $config['connection_options']['port'] . $separator;
+            $dsn .= 'dbname=' . $config['connection_options']['dbname'] . $separator;
+            $dsn = rtrim($dsn);
+            $user = $config['connection_options']['user'];
+            $password = $config['connection_options']['password'];
+            $connection = new PDO($dsn, $user, $password);
+        }
+
+        return $connection;
+    }
+}
---
+++ tests/AllStreamProjectionTest.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * This file is part of the prooph/standard-projections.
+ * (c) 2016-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\StandardProjections;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Prooph\Common\Event\ProophActionEventEmitter;
+use Prooph\EventStore\InMemoryEventStore;
+use Prooph\EventStore\Projection\InMemoryEventStoreProjection;
+use Prooph\EventStore\Stream;
+use Prooph\EventStore\StreamName;
+use Prooph\StandardProjections\AllStreamProjection;
+use ProophTest\EventStore\Mock\TestDomainEvent;
+
+class AllStreamProjectionTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_emits_events_for_all_streams()
+    {
+        $eventStore = new InMemoryEventStore(new ProophActionEventEmitter());
+
+        $eventStore->beginTransaction();
+
+        $eventStore->create(
+            new Stream(
+                new StreamName('foo'),
+                new \ArrayIterator([
+                    TestDomainEvent::with(['1'], 1),
+                    TestDomainEvent::with(['2'], 2),
+                ])
+            )
+        );
+
+        $eventStore->create(
+            new Stream(
+                new StreamName('bar'),
+                new \ArrayIterator([
+                    TestDomainEvent::with(['a'], 1),
+                    TestDomainEvent::with(['b'], 2),
+                ])
+            )
+        );
+
+        $eventStore->appendTo(
+            new StreamName('foo'),
+            new \ArrayIterator([
+                TestDomainEvent::with(['3'], 3),
+                TestDomainEvent::with(['4'], 4),
+            ])
+        );
+
+        $eventStore->appendTo(
+            new StreamName('bar'),
+            new \ArrayIterator([
+                TestDomainEvent::with(['c'], 3),
+                TestDomainEvent::with(['d'], 4),
+            ])
+        );
+
+        $eventStore->commit();
+
+        $projection = new InMemoryEventStoreProjection($eventStore, '$all', true);
+
+        $allStreamProjection = new AllStreamProjection(
+            $projection
+        );
+        $allStreamProjection();
+
+        $this->assertTrue($eventStore->hasStream(new StreamName('$all')));
+
+        $stream = $eventStore->load(new StreamName('$all'));
+
+        $streamEvents = $stream->streamEvents();
+
+        $this->assertCount(8, $streamEvents);
+
+        $event = $streamEvents->current();
+
+        $this->assertEquals(['1'], $event->payload());
+
+        $streamEvents->next();
+        $event = $streamEvents->current();
+
+        $this->assertEquals(['2'], $event->payload());
+
+        $streamEvents->next();
+        $event = $streamEvents->current();
+
+        $this->assertEquals(['3'], $event->payload());
+
+        $streamEvents->next();
+        $event = $streamEvents->current();
+
+        $this->assertEquals(['4'], $event->payload());
+
+        $streamEvents->next();
+        $event = $streamEvents->current();
+
+        $this->assertEquals(['a'], $event->payload());
+
+        $streamEvents->next();
+        $event = $streamEvents->current();
+
+        $this->assertEquals(['b'], $event->payload());
+
+        $streamEvents->next();
+        $event = $streamEvents->current();
+
+        $this->assertEquals(['c'], $event->payload());
+
+        $streamEvents->next();
+        $event = $streamEvents->current();
+
+        $this->assertEquals(['d'], $event->payload());
+    }
+}
